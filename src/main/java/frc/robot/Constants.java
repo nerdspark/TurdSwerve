e508022285eb6ffe6ce2a4b1dc1ab00457b29e9b
maxwell.lee.372007 <maxwell.lee.372007@gmail.com>
2023-12-15 19:21:17 -0500

    initialize motors and CAN ID constants

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,18 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/** Add your docs here. */
+public final class Constants {
+    public final int leftAzimuthID = 0;
+    public final int rightAzimuthID = 2;
+
+    public final int leftDriveID = 1;
+    public final int rightDriveID = 3;
+
+    public final int leftEncoderID = 0;
+    public final int rightEncoderID = 1;
+
+}
--- src/main/java/frc/robot/subsystems/TurdPod.java
+++ src/main/java/frc/robot/subsystems/TurdPod.java
@@ -4,11 +4,27 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.PWM;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class TurdPod extends SubsystemBase {
-  /** Creates a new TurdPod. */
-  public TurdPod() {}
+
+  private final CANSparkMax azimuth;
+  private final CANSparkMax drive;
+  private final PWM encoder;
+
+  public TurdPod(int azimuthID, int driveID, int encoderID) {
+    azimuth = new CANSparkMax(azimuthID, MotorType.kBrushless);
+    drive = new CANSparkMax(driveID, MotorType.kBrushless);
+    encoder = new PWM(encoderID);
+  }
+
+
 
   @Override
   public void periodic() {
