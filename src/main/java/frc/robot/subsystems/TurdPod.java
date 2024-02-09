13ed20b8c657275bd4ae9d1fa13f1219a4e0f18d
Jayden Liu <reinforcedphantom@gmail.com>
2024-02-08 20:42:20 -0500

    advantage kit

--- src/main/java/frc/robot/subsystems/TurdPod.java
+++ src/main/java/frc/robot/subsystems/TurdPod.java
@@ -26,7 +26,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-public class TurdPod extends SubsystemBase {
+public class TurdPod implements TurdSwerveIO {
 
   private final CANSparkMax azimuth;
   private final CANSparkMax drive;
---
+++ src/main/java/frc/robot/subsystems/TurdSwerveIO.java
@@ -0,0 +1,19 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import org.littletonrobotics.junction.AutoLog;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public interface TurdSwerveIO {
+  class TurdSwerveIOInputs {
+    @AutoLog
+    public static double 
+  }
+
+  
+}
+}
