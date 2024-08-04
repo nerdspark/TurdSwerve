d2e06fc171d2b1fc3577ba2a72c7a7aac94563d5
OrigamiYoda <52184332+OrigamiYoda@users.noreply.github.com>
2024-08-04 11:31:09 -0400

    chrp

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,11 +7,14 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.Concert;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
+
+    private Concert m_concert;
 
   @Override
   public void robotInit() {
@@ -58,7 +61,9 @@ public class Robot extends TimedRobot {
   public void teleopPeriodic() {}
 
   @Override
-  public void teleopExit() {}
+  public void teleopExit() {
+    m_concert = new Concert();
+  }
 
   @Override
   public void testInit() {
