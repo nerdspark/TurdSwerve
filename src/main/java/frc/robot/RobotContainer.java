3826368e5867b8a05d759a5fa445742c02b2152c
OrigamiYoda <52184332+OrigamiYoda@users.noreply.github.com>
2025-03-10 23:01:26 -0400

    Choreo marker test

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -70,7 +70,7 @@ public class RobotContainer {
     }
 
     private void configureNamedCommands() {
-      NamedCommands.registerCommand("printTest", Commands.print("[Path Planner] Marker Auto Action Test"));
+      NamedCommands.registerCommand("printTest", Commands.print("[Path Planner Auto with Choreo Path] Marker Auto Action Test"));
       NamedCommands.registerCommand("printTestTeleop", Commands.print("[Path Planner] Marker Teleop Auto Action Test"));
     }
 
